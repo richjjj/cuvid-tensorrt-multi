9536e9caec593e2c2dffd9ff1e6d650dccc8f78b
zc <zc@xmrbi.com>
2023-07-11 11:52:32 +0800

    add classify

---
+++ src/application/app_classify/classify.hpp
@@ -0,0 +1,12 @@
+#ifndef CLASSIFY_HPP
+#define CLASSIFY_HPP
+
+#include <vector>
+#include <memory>
+#include <string>
+#include <future>
+#include <opencv2/opencv.hpp>
+
+namespace Classify {};  // namespace Classify
+
+#endif
